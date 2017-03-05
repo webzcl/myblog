6fc7e9a2ead356f1d107741cde9d286266bf8a93
zcl <810301312@qq.com>
2017-03-05 14:34:32 +0800

    my branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routers.js';
-import 'highlight.js/styles/arta.css';
-import './main.css';
-import './post.css';
-
-render(<Routers />,document.getElementById('root'));
