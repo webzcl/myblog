00e7fc2c408ea9896979b290cbb808dcdee28bb9
zcl <810301312@qq.com>
2017-03-05 09:57:19 +0800

    biaoti

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import axios from 'axios';
+import marked from 'marked';
+
+
 
 class Post extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      data:''
+    }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/webzcl/myblog/master/Blogs/${this.props.params.title}.md`)
+    .then(res=>this.setState({data:res.data}))
+  }
   render () {
     return(
       <div>
-        {this.props.params.title}
+        {this.state.data.length==0 ? '正在加载中':
+        <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>
+      }
       </div>
     )
   }
 }
+export default Post;
+
+
+
+
+
+
 // let Post =function({params}=props){   //结构赋值
 //   console.log(props);
 //   return(
 //     <div>par</div>
 //   )
 // }
-export default Post;
